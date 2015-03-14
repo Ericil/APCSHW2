0fb52a3753d3975d051c666bcd7bad182c651370
Cookie <mingrui.li3@gmail.com>
2015-03-14 17:15:02 -0400

    fixed add and remove to take care of adding and removing at index 0

--- 07Linkedlist/MyLinkedList.java
+++ 07Linkedlist/MyLinkedList.java
@@ -4,6 +4,7 @@ public class MyLinkedList{
     LNode start = new LNode(null, 0);
     int location;
     int thesize = 0;
+    LNode end;
     public String toString(){
 	LNode place = start;
 	String data = "";
@@ -67,49 +68,62 @@ public class MyLinkedList{
 	}
 	LNode newnode = new LNode(null, value);
 	LNode place = start;
-	while(location != index - 1){
-	    place = place.getNext();
-	    location++;
+	if (index > 0){
+	    LNode previous = start;
+	    while(location != index){
+		if (location == index - 1){
+		    previous = place;
+		}
+		place = place.getNext();
+		location++;
+	    }
+	    previous.setNext(newnode);
+	    newnode.setNext(place);
+	}else{
+	    newnode.setNext(place);
+	    start = newnode;
 	}
-	LNode place2 = place.getNext();
-	place.setNext(newnode);
-	place = place.getNext();
-	place.setNext(place2);
 	thesize++;
+	if (location == thesize - 1){
+	    end = place;
+	}
 	
     }
-
+    
     public int remove(int index){
 	location = 0;
+	int returning = start.getData();
 	if (index < 0 || index > thesize){
 	    throw new IndexOutOfBoundsException("index under 0 or above the size");
 	}
-	LNode place = start;
-	while(location != index - 1){
+	if (index > 0){
+	    LNode place = start;
+	    while(location != index - 1){
+		place = place.getNext();
+		location++;
+	    }
+	    LNode hold = place;
 	    place = place.getNext();
-	    location++;
+	    returning = place.getData();
+	    LNode hold2 = place.getNext();
+	    hold.setNext(hold2);
+	    thesize--;
+	    if (hold.getNext() == null){
+	    end = hold;
+	    }
+	}else{
+	    LNode place = start.getNext();
+	    start = place;
+	    thesize--;
 	}
-	LNode hold = place;
-	place = place.getNext();
-	int returning = place.getData();
-	LNode hold2 = place.getNext();
-	hold.setNext(hold2);
-	thesize--;
 	return returning;
-	
-	
     }
+    
+	
+    
 
     public int size(){
-	LNode place = start;
-	int thesize = 0;
-	if (place.getNext() != null){
-	    while (place.getNext() != null){
-		place = place.getNext();
-		thesize++;
-	    }
-	}
-	return thesize;
+        return thesize;
 	    
     }
     
@@ -132,13 +146,12 @@ public class MyLinkedList{
     public static void main(String[]args){
 	MyLinkedList a = new MyLinkedList();
 	a.add(1);
-	a.add(2);
-	a.add(3);
 	System.out.println(a);
-	a.add(3, 4);
-        a.set(2, 5);
+	a.add(0, 4);
 	System.out.println(a);
-	a.remove(1);
+	a.add(5);
+	System.out.println(a);
+        a.remove(0);
 	System.out.println(a);
 	System.out.println(a.indexOf(10));
     }
