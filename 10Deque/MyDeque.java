15af9068b3654a92379cacbeab409e64ce83b566
Cookie <mingrui.li3@gmail.com>
2015-03-25 12:34:03 -0400

    resize made, not sure if it works

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,14 +1,38 @@
+import java.util.*;
 public class MyDeque<T>{
     int head;
     int tail;
     Object[] list;
+    int size;
     public MyDeque(){
-	list = new Object[100];
+	list = new Object[10];
 	head = 49;
-	tail = 49;
+	tail = 50;
+	size = 0;
     }
-    public void helper(){
-	if (head == 
-    public void addFirst(T value){
+    public void meow(){
+	if (size == list.length){
+	    Object[] list2 = new Object[size * 2];
+	    int trigger = 0;
+	    int place = head + 1;
+	    for (int a = (size * 2)/4;trigger < 0; trigger++){
+		if (place == list.length - 1){
+		    place = 0;
+		}
+	        list2[a] = list[place];
+		place++;
+		a++;
+	    }
+	    list = list2;
+	}
+    }
+    public static void main(String[]args){
+	MyDeque<Integer> a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	a.size = 10;
+	a.meow();
+       
+	System.out.println(Arrays.toString(a));
+    }
+}
 	
     
