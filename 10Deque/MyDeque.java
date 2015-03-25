3d8fe1979ac37d7cef503e02bfb98546b0c3e680
Cookie <mingrui.li3@gmail.com>
2015-03-25 12:13:18 -0400

    started

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,14 @@
+public class MyDeque<T>{
+    int head;
+    int tail;
+    Object[] list;
+    public MyDeque(){
+	list = new Object[100];
+	head = 49;
+	tail = 49;
+    }
+    public void helper(){
+	if (head == 
+    public void addFirst(T value){
+	
+    
