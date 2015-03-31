623b2bce09339c4c322c335c9ac0032f63f6cf37
Ericil <mingrui.li3@gmail.com>
2015-03-31 12:33:09 -0400

    set up the file

--- 11BFS/Maze.java
+++ 11BFS/Maze.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Mazesolver{
+public class Maze{
     private char[][]maze;
     private int maxx,maxy;
     private int startx,starty;
@@ -10,7 +10,7 @@ public class Mazesolver{
     private String hide =  "\033[?25l";
     private String show =  "\033[?25h";
 
-    public Mazesolver(String filename){
+    public Maze(String filename){
 	startx = -1;
 	starty = -1;
 	//read the whole maze into a single string first
@@ -85,19 +85,42 @@ public class Mazesolver{
 	}
 	return hide + go(0,0) + ans + "\n" + show + color(37,40);
     }
-   
+
+    public String toString();//do not do the funky character codes
+    
+    public String toString(boolean animate); //do the funky character codes when animate is true
+
+    /**Solve the maze using a frontier in a BFS manner. 
+     * When animate is true, print the board at each step of the algorithm.
+     * Replace spaces with x's as you traverse the maze. 
+     */
+    public boolean solveBFS(boolean animate){    }
+
+    /**Solve the maze using a frontier in a DFS manner. 
+     * When animate is true, print the board at each step of the algorithm.
+     * Replace spaces with x's as you traverse the maze. 
+     */
+    public boolean solveDFS(boolean animate){    }
+    
+    public boolean solveBFS(){
+	return solveBFS(false);
+    }
+    public boolean solveDFS(){
+	return solveDFS(false);
+    }
    
     public static void main(String[]args){
 	Mazesolver f;
 	if(args.length < 1){
-	    f = new Mazesolver("data1.dat");
+	    f = new Maze("data1.dat");
 	}else{
-	    f = new Mazesolver(args[0]);
+	    f = new Maze(args[0]);
 	}
 	f.clearTerminal();
 	f.solve();
 	
     }
+    
     
     
 }
